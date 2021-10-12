a145d4e51a9cf1785316d6c99dcf0da0ad9ea4ef
gourav <gouravpunani222@gmail.com>
2021-10-12 19:13:01 +0530

    synce file fixed

--- sync-employees.php
+++ sync-employees.php
@@ -1,19 +1,25 @@
 <?php
 
+require_once dirname(dirname(__FILE__))."/wp-load.php";
+
 $technicians = (new Technician_details)->get_all_technicians();
 echo "<pre>";
 echo "total ".count($technicians);
 foreach($technicians as $tech){
 
     $employee_data = [
-        'employee_ref_id'   =>  $tech->id,
-        'name'              =>  $tech->first_name." ".$tech->last_name,
-        'email'             =>  $tech->email,
-        'address'           =>  $tech->address,
-        'role_id'           =>  '1',
-        'username'          =>  $tech->slug,
-        'password'          =>  $tech->password
-        ];
+        'employee_ref_id'       =>  $tech->id,
+        'name'                  =>  $tech->first_name." ".$tech->last_name,
+        'email'                 =>  $tech->email,
+        'address'               =>  $tech->address,
+        'role_id'               =>  '1',
+        'username'              =>  $tech->slug,
+        'password'              =>  $tech->password,
+        'application_status'    =>  $tech->application_status,
+        'status'                =>  $tech->status
+    ];
+
+
 
     $response = (new Employee\Employee)->createEmployee($employee_data);   
 
@@ -27,55 +33,19 @@ echo "<pre>";
 echo "total ".count($cold_callers);
 foreach ($cold_callers as $cold_caller) {
     $employee_data = [
-        'employee_ref_id'   =>  $cold_caller->id,
-        'name'              =>  $cold_caller->name,
-        'email'             =>  $cold_caller->email,
-        'address'           =>  $cold_caller->address,
-        'phone_no'          =>  $cold_caller->phone_no,
-        'role_id'           =>  '2',
-        'username'          =>  $cold_caller->username,
-        'password'          =>  $cold_caller->password
+        'employee_ref_id'       =>  $cold_caller->id,
+        'name'                  =>  $cold_caller->name,
+        'email'                 =>  $cold_caller->email,
+        'address'               =>  $cold_caller->address,
+        'phone_no'              =>  $cold_caller->phone_no,
+        'role_id'               =>  '2',
+        'username'              =>  $cold_caller->username,
+        'password'              =>  $cold_caller->password,
+        'application_status'    =>  $cold_caller->application_status,
     ];
 
-    $response = (new Employee\Employee)->createEmployee($employee_data);   
-
-    if(!$response){
-        print_r($employee_data);
-    }
-}
-
-
-$office_staff = (new OfficeStaff)->getStaffMembers();
-echo "<pre>";
-echo "total ".count($office_staff);
-foreach ($office_staff as $staff_member) {
-    
-    $employee_data = [
-        'employee_ref_id'   =>  $staff_member->id,
-        'name'              =>  $staff_member->name,
-        'email'             =>  $staff_member->email,
-        'address'           =>  $staff_member->address,
-        'role_id'           =>  '3',
-        'username'          =>  $staff_member->username,
-        'password'          =>  $staff_member->password
-    ];
-
-    $response = (new Employee\Employee)->createEmployee($employee_data);   
-
-$door_to_door_employees = (new Employee\DoorToDoorSales)->get();
-echo "<pre>";
-echo "total ".count($door_to_door_employees);
-foreach ($door_to_door_employees as $door_to_door) {
-    
-    $employee_data = [
-        'employee_ref_id'   =>  $door_to_door->id,
-        'name'              =>  $door_to_door->name,
-        'email'             =>  $door_to_door->email,
-        'address'           =>  $door_to_door->address,
-        'role_id'           =>  '3',
-        'username'          =>  $door_to_door->username,
-        'password'          =>  $door_to_door->password
-    ];
+    if($cold_caller->status == "inactive") $employee_data['status'] = 0;
+    if($cold_caller->status == "active") $employee_data['status'] = 1;
 
     $response = (new Employee\Employee)->createEmployee($employee_data);   
 
