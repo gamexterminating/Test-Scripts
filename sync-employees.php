2646dacea3fc035944a819c856dd07d947e642a8
Gourav Punani <gouravpunani@ip-172-26-3-166.us-east-2.compute.internal>
2021-10-12 09:43:41 -0400

    live changes

--- sync-employees.php
+++ sync-employees.php
@@ -22,64 +22,65 @@ foreach($technicians as $tech){
     }
 }
 
-$cold_callers = (new ColdCaller)->getAllColdCallers();
-echo "<pre>";
-echo "total ".count($cold_callers);
-foreach ($cold_callers as $cold_caller) {
-    $employee_data = [
-        'employee_ref_id'   =>  $cold_caller->id,
-        'name'              =>  $cold_caller->name,
-        'email'             =>  $cold_caller->email,
-        'address'           =>  $cold_caller->address,
-        'phone_no'          =>  $cold_caller->phone_no,
-        'role_id'           =>  '2',
-        'username'          =>  $cold_caller->username,
-        'password'          =>  $cold_caller->password
-    ];
+// $cold_callers = (new ColdCaller)->getAllColdCallers();
+// echo "<pre>";
+// echo "total ".count($cold_callers);
+// foreach ($cold_callers as $cold_caller) {
+//     $employee_data = [
+//         'employee_ref_id'   =>  $cold_caller->id,
+//         'name'              =>  $cold_caller->name,
+//         'email'             =>  $cold_caller->email,
+//         'address'           =>  $cold_caller->address,
+//         'phone_no'          =>  $cold_caller->phone_no,
+//         'role_id'           =>  '2',
+//         'username'          =>  $cold_caller->username,
+//         'password'          =>  $cold_caller->password
+//     ];
 
-    $response = (new Employee\Employee)->createEmployee($employee_data);   
+//     $response = (new Employee\Employee)->createEmployee($employee_data);   
 
-    if(!$response){
-        print_r($employee_data);
-    }
-}
+//     if(!$response){
+//         print_r($employee_data);
+//     }
+// }
 
 
-$office_staff = (new OfficeStaff)->getStaffMembers();
-echo "<pre>";
-echo "total ".count($office_staff);
-foreach ($office_staff as $staff_member) {
+// $office_staff = (new OfficeStaff)->getStaffMembers();
+// echo "<pre>";
+// echo "total ".count($office_staff);
+// foreach ($office_staff as $staff_member) {
     
-    $employee_data = [
-        'employee_ref_id'   =>  $staff_member->id,
-        'name'              =>  $staff_member->name,
-        'email'             =>  $staff_member->email,
-        'address'           =>  $staff_member->address,
-        'role_id'           =>  '3',
-        'username'          =>  $staff_member->username,
-        'password'          =>  $staff_member->password
-    ];
+//     $employee_data = [
+//         'employee_ref_id'   =>  $staff_member->id,
+//         'name'              =>  $staff_member->name,
+//         'email'             =>  $staff_member->email,
+//         'address'           =>  $staff_member->address,
+//         'role_id'           =>  '3',
+//         'username'          =>  $staff_member->username,
+//         'password'          =>  $staff_member->password
+//     ];
 
-    $response = (new Employee\Employee)->createEmployee($employee_data);   
+//     $response = (new Employee\Employee)->createEmployee($employee_data);
+// }   
 
-$door_to_door_employees = (new Employee\DoorToDoorSales)->get();
-echo "<pre>";
-echo "total ".count($door_to_door_employees);
-foreach ($door_to_door_employees as $door_to_door) {
+// $door_to_door_employees = (new Employee\DoorToDoorSales)->get();
+// echo "<pre>";
+// echo "total ".count($door_to_door_employees);
+// foreach ($door_to_door_employees as $door_to_door) {
     
-    $employee_data = [
-        'employee_ref_id'   =>  $door_to_door->id,
-        'name'              =>  $door_to_door->name,
-        'email'             =>  $door_to_door->email,
-        'address'           =>  $door_to_door->address,
-        'role_id'           =>  '3',
-        'username'          =>  $door_to_door->username,
-        'password'          =>  $door_to_door->password
-    ];
+//     $employee_data = [
+//         'employee_ref_id'   =>  $door_to_door->id,
+//         'name'              =>  $door_to_door->name,
+//         'email'             =>  $door_to_door->email,
+//         'address'           =>  $door_to_door->address,
+//         'role_id'           =>  '3',
+//         'username'          =>  $door_to_door->username,
+//         'password'          =>  $door_to_door->password
+//     ];
 
-    $response = (new Employee\Employee)->createEmployee($employee_data);   
+//     $response = (new Employee\Employee)->createEmployee($employee_data);   
 
-    if(!$response){
-        print_r($employee_data);
-    }
-}
+//     if(!$response){
+//         print_r($employee_data);
+//     }
+// }
